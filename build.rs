45918c57b91a98d917a9575c0cb7f018e5f79a84
Timothy <tim@openenergysolutionsinc.com>
2022-08-16 15:41:37 -0700

    Add submodule dependencies refactor build script and delete dss_capi folder
    Signed-off-by: Timothy <tim@openenergysolutionsinc.com>

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use cmake;
 use std::{env, fs, path};
 
 fn main() {
-    let mut klusolvex_config = cmake::Config::new("dss_capi/klusolvex");
+    let mut klusolvex_config = cmake::Config::new("klusolve");
     klusolvex_config.define("USE_SYSTEM_SUITESPARSE", "OFF"); // FIXME: should be "ON", but causes failure..
     klusolvex_config.define("USE_SYSTEM_EIGEN3", "ON");
 
@@ -11,14 +11,14 @@ fn main() {
     klusolvex_config.define("CMAKE_BUILD_TYPE", "Release");
     klusolvex_config.build();
 
-    let mut klusolvex_config = cmake::Config::new("dss_capi/klusolvex");
+    let mut klusolvex_config = cmake::Config::new("klusolve");
     klusolvex_config.configure_arg("--build");
     klusolvex_config.configure_arg(".");
     klusolvex_config.build();
 
-    let klusolvex_lib_src = "dss_capi/klusolvex/lib/linux_x64/libklusolvexd.so";
-    let klusolvex_lib_dst = "dss_capi/lib/linux_x64/libklusolvex.so";
-    fs::copy(klusolvex_lib_src, klusolvex_lib_dst).expect("Failed to write to dss_capi/lib/linu_x64");
+    let klusolve_lib_src = "klusolve/lib/linux_x64/libklusolvexd.so";
+    let klusolve_lib_dst = "dss_capi/lib/linux_x64/libklusolvex.so";
+    fs::copy(klusolve_lib_src, klusolve_lib_dst).expect("Failed to write to dss_capi/lib/linux_x64");
 
     let bindings = bindgen::Builder::default()
         .header("dss_capi/include/dss_capi.h")
